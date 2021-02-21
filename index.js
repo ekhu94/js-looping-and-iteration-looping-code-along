12303bac470a2f2f7e79ed2ebd77061529dfae40
Erik Huang <erikhuangdrums@gmail.com>
2021-02-21 07:08:28 -0800

    commit tree e244ce8e6356c3da32efc185ebab7c12af9572a4

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const writeCards = (arr, name) => {
+    const output = [];
+    for (let i = 0; i < arr.length; i++) {
+        output.push(`Thank you, ${arr[i]}, for the wonderful ${name} gift!`);
+    }
+    return output;
+}
+
+const countDown = n => {
+    while (n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
